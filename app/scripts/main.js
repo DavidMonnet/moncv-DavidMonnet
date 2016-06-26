d5c2bfabe9e64933a074fb1c67ce7316b1666dc2
DavidMonnet <DaveMo0333@gmail.com>
2016-06-26 13:27:05 +0200

    Update progress bars to charts
    Update progress bars to automatically generated charts
    Remove simple charts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,37 +3,37 @@ $(document).ready(function(){
 	'use strict';
 	$('a').smoothScroll();
 	
-	/*AFFICHAGE DU CHARTS SIMPLES*/ 
-	//Doughnut chart
-	var data = {
-		labels: [
-			'What I already know',
-		],
-		datasets: [
-			{
-				data: [80, 20],
-				backgroundColor: [
-					'#0258D1',  //#36A2EB (bleu plus clair)
-					'#f4f4f4'
-				],
-				hoverBackgroundColor: [
-					'#0258D1',
-					'#f4f4f4'
-				],
-				hoverBorderColor: [
-					'#f4f4f4',
-					'#f4f4f4'
-				],
-				borderColor:[
-					'#f4f4f4',
-					'#f4f4f4'
-				],
-			}]
-	};
-	var cty = document.getElementById('myChartDoughnut');
-	var myDoughnutChart = new Chart(cty, {
-		type: 'doughnut',
-		data: data,
-		//options: options
+	/*GENERATION DU CHART A PARTIR DE LA PROGRESS-BAR*/
+	$('.progress-bar').each(function(){
+		var val= $(this).attr('aria-valuenow');  //on stock la valeur de aria-valuenow --> le pourcentage rempli sur la progress bar
+		var color= $(this).css('background-color'); //on récupère la couleur actuel de la progress bar parcouru
+		var canvas = $('<canvas> </canvas>');  //création du canvas pour le donut
+		$(this).parent().replaceWith(canvas);  //on était actuellement au niveau enfant, on doit mtn remonter pour accéder au niveau parent et remplacer tout ce bloc (<div class="progress">)
+	
+		var myOtherDoughnutChart = new Chart(canvas, {
+			type: 'doughnut',
+			data: {
+				labels: [val + ' %'],  //? à tester
+				datasets: [{
+						data: [val, 100-val],
+						backgroundColor: [
+							color, //"#0258D1",  //#36A2EB (bleu plus clair)
+							'#f4f4f4'
+						],
+						hoverBackgroundColor: [
+							color, //"#0258D1",
+							'#f4f4f4'
+						],
+						hoverBorderColor: [
+							'#f4f4f4',
+							'#f4f4f4'
+						],
+						borderColor:[
+							'#f4f4f4',
+							'#f4f4f4'
+						]
+				}]
+			}
+		});
 	});
 });
