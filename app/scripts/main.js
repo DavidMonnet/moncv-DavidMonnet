e75d06f656a9c604d340f8b14bdd38d19ab12afa
DavidMonnet <DaveMo0333@gmail.com>
2016-06-26 12:57:11 +0200

    Add Simple Doughnut Chart in skills
    Add Simple Doughnut Chart in skills

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,39 @@
-$( document ).ready(function() {
+$(document).ready(function(){
 	/*SMOOTH-SCROLL-DOWN*/
 	'use strict';
 	$('a').smoothScroll();
+	
+	/*AFFICHAGE DU CHARTS SIMPLES*/ 
+	//Doughnut chart
+	var data = {
+		labels: [
+			'What I already know',
+		],
+		datasets: [
+			{
+				data: [80, 20],
+				backgroundColor: [
+					'#0258D1',  //#36A2EB (bleu plus clair)
+					'#f4f4f4'
+				],
+				hoverBackgroundColor: [
+					'#0258D1',
+					'#f4f4f4'
+				],
+				hoverBorderColor: [
+					'#f4f4f4',
+					'#f4f4f4'
+				],
+				borderColor:[
+					'#f4f4f4',
+					'#f4f4f4'
+				],
+			}]
+	};
+	var cty = document.getElementById('myChartDoughnut');
+	var myDoughnutChart = new Chart(cty, {
+		type: 'doughnut',
+		data: data,
+		//options: options
+	});
 });
